77963004ae4ea9376a60de6a881d42e8724362bc
Alex Kobylansky <alex.kobylansky@gmail.com>
2025-08-31 18:19:30 +0300

    Replace quotes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import type React from "react"
-import "./[locale]/globals.css"
-import {GeistSans} from "geist/font/sans";
-import {GeistMono} from "geist/font/mono";
+import type React from 'react'
+import './[locale]/globals.css'
+import {GeistSans} from 'geist/font/sans';
+import {GeistMono} from 'geist/font/mono';
 
 export default function RootLayout({
                                      children,
@@ -23,7 +23,7 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
-      <title>{locale == "en" ? "Forecast Website" : "Сайт погоди"}</title>
+      <title>{locale == 'en' ? 'Forecast Website' : 'Сайт погоди'}</title>
     </head>
     <body>
     {children}
