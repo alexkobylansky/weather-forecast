5f2e5323cf7b55d8b25f9972be2b3ca2e3f3eadd
Alex Kobylansky <alex.kobylansky@gmail.com>
2025-10-27 20:42:01 +0200

    Added components DaysTabComponent

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -498,221 +498,36 @@ export default function WeatherForecast() {
                       }
                     })}
                   </TabsList>
-                  <TabsContent value='day1' className='mt-6'>
-                    <Card>
-                      <CardContent className='p-0'>
-                        <Table>
-                          <TableHeader>
-                            <TableRow>
-                              <TableHead>{t('table.time')}</TableHead>
-                              <TableHead>{t('table.weather')}</TableHead>
-                              <TableHead>{t('table.temperature')} (&deg;C)</TableHead>
-                              <TableHead>{t('table.feelsLike')}(&deg;C)</TableHead>
-                              <TableHead>{`${t('table.wind')} (${t('table.windSpeed')})`}</TableHead>
-                              <TableHead>{t('table.windDirection')}</TableHead>
-                              <TableHead>{t('table.windGust')}</TableHead>
-                              <TableHead>{t('table.humidity')}</TableHead>
-                              <TableHead>{t('table.pressure')} (mmHg)</TableHead>
-                              <TableHead>{`${t('table.visibility')} (${t('table.visibilityDistance')})`}</TableHead>
-                            </TableRow>
-                          </TableHeader>
-                          <TableBody>
-                            {day1.map((day: listItem, index) => (
-                              <TableRow key={index}>
-                                <TableCell className='font-medium'>{timestampConversation(day.dt)}</TableCell>
-                                <TableCell>
-                                  <div className='flex items-center space-x-2'>
-                                    {showIcon(day.weather[0].id, day.dt,'h-5 w-5 text-primary')}
-                                    <span>{day.weather[0].description}</span>
-                                  </div>
-                                </TableCell>
-                                <TableCell className='font-semibold'>{Math.round(day.main.temp)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.main.feels_like)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.wind.speed)}</TableCell>
-                                <TableCell>{windDeg(day.wind.deg)}</TableCell>
-                                <TableCell>{day.wind.gust}</TableCell>
-                                <TableCell>{day.main.humidity}%</TableCell>
-                                <TableCell>{Math.floor((day.main.pressure * 0.75006156) * 100) / 100}</TableCell>
-                                <TableCell>{day.visibility / 1000}</TableCell>
-                              </TableRow>
-                            ))}
-                          </TableBody>
-                        </Table>
-                      </CardContent>
-                    </Card>
-                  </TabsContent>
-                  <TabsContent value='day2' className='mt-6'>
-                    <Card>
-                      <CardContent className='p-0'>
-                        <Table>
-                          <TableHeader>
-                            <TableRow>
-                              <TableHead>{t('table.time')}</TableHead>
-                              <TableHead>{t('table.weather')}</TableHead>
-                              <TableHead>{t('table.temperature')} (&deg;C)</TableHead>
-                              <TableHead>{t('table.feelsLike')}(&deg;C)</TableHead>
-                              <TableHead>{`${t('table.wind')} (${t('table.windSpeed')})`}</TableHead>
-                              <TableHead>{t('table.windDirection')}</TableHead>
-                              <TableHead>{t('table.windGust')}</TableHead>
-                              <TableHead>{t('table.humidity')}</TableHead>
-                              <TableHead>{t('table.pressure')} (mmHg)</TableHead>
-                              <TableHead>{`${t('table.visibility')} (${t('table.visibilityDistance')})`}</TableHead>
-                            </TableRow>
-                          </TableHeader>
-                          <TableBody>
-                            {day2.map((day: listItem, index) => (
-                              <TableRow key={index}>
-                                <TableCell className='font-medium'>{timestampConversation(day.dt)}</TableCell>
-                                <TableCell>
-                                  <div className='flex items-center space-x-2'>
-                                    {showIcon(day.weather[0].id, day.dt,'h-5 w-5 text-primary')}
-                                    <span>{day.weather[0].description}</span>
-                                  </div>
-                                </TableCell>
-                                <TableCell className='font-semibold'>{Math.round(day.main.temp)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.main.feels_like)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.wind.speed)}</TableCell>
-                                <TableCell>{windDeg(day.wind.deg)}</TableCell>
-                                <TableCell>{day.wind.gust}</TableCell>
-                                <TableCell>{day.main.humidity}%</TableCell>
-                                <TableCell>{Math.floor((day.main.pressure * 0.75006156) * 100) / 100}</TableCell>
-                                <TableCell>{day.visibility / 1000}</TableCell>
-                              </TableRow>
-                            ))}
-                          </TableBody>
-                        </Table>
-                      </CardContent>
-                    </Card>
-                  </TabsContent>
-                  <TabsContent value='day3' className='mt-6'>
-                    <Card>
-                      <CardContent className='p-0'>
-                        <Table>
-                          <TableHeader>
-                            <TableRow>
-                              <TableHead>{t('table.time')}</TableHead>
-                              <TableHead>{t('table.weather')}</TableHead>
-                              <TableHead>{t('table.temperature')} (&deg;C)</TableHead>
-                              <TableHead>{t('table.feelsLike')}(&deg;C)</TableHead>
-                              <TableHead>{`${t('table.wind')} (${t('table.windSpeed')})`}</TableHead>
-                              <TableHead>{t('table.windDirection')}</TableHead>
-                              <TableHead>{t('table.windGust')}</TableHead>
-                              <TableHead>{t('table.humidity')}</TableHead>
-                              <TableHead>{t('table.pressure')} (mmHg)</TableHead>
-                              <TableHead>{`${t('table.visibility')} (${t('table.visibilityDistance')})`}</TableHead>
-                            </TableRow>
-                          </TableHeader>
-                          <TableBody>
-                            {day3.map((day: listItem, index) => (
-                              <TableRow key={index}>
-                                <TableCell className='font-medium'>{timestampConversation(day.dt)}</TableCell>
-                                <TableCell>
-                                  <div className='flex items-center space-x-2'>
-                                    {showIcon(day.weather[0].id, day.dt,'h-5 w-5 text-primary')}
-                                    <span>{day.weather[0].description}</span>
-                                  </div>
-                                </TableCell>
-                                <TableCell className='font-semibold'>{Math.round(day.main.temp)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.main.feels_like)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.wind.speed)}</TableCell>
-                                <TableCell>{windDeg(day.wind.deg)}</TableCell>
-                                <TableCell>{day.wind.gust}</TableCell>
-                                <TableCell>{day.main.humidity}%</TableCell>
-                                <TableCell>{Math.floor((day.main.pressure * 0.75006156) * 100) / 100}</TableCell>
-                                <TableCell>{day.visibility / 1000}</TableCell>
-                              </TableRow>
-                            ))}
-                          </TableBody>
-                        </Table>
-                      </CardContent>
-                    </Card>
-                  </TabsContent>
-                  <TabsContent value='day4' className='mt-6'>
-                    <Card>
-                      <CardContent className='p-0'>
-                        <Table>
-                          <TableHeader>
-                            <TableRow>
-                              <TableHead>{t('table.time')}</TableHead>
-                              <TableHead>{t('table.weather')}</TableHead>
-                              <TableHead>{t('table.temperature')} (&deg;C)</TableHead>
-                              <TableHead>{t('table.feelsLike')}(&deg;C)</TableHead>
-                              <TableHead>{`${t('table.wind')} (${t('table.windSpeed')})`}</TableHead>
-                              <TableHead>{t('table.windDirection')}</TableHead>
-                              <TableHead>{t('table.windGust')}</TableHead>
-                              <TableHead>{t('table.humidity')}</TableHead>
-                              <TableHead>{t('table.pressure')} (mmHg)</TableHead>
-                              <TableHead>{`${t('table.visibility')} (${t('table.visibilityDistance')})`}</TableHead>
-                            </TableRow>
-                          </TableHeader>
-                          <TableBody>
-                            {day4.map((day, index) => (
-                              <TableRow key={index}>
-                                <TableCell className='font-medium'>{timestampConversation(day.dt)}</TableCell>
-                                <TableCell>
-                                  <div className='flex items-center space-x-2'>
-                                    {showIcon(day.weather[0].id, day.dt,'h-5 w-5 text-primary')}
-                                    <span>{day.weather[0].description}</span>
-                                  </div>
-                                </TableCell>
-                                <TableCell className='font-semibold'>{Math.round(day.main.temp)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.main.feels_like)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.wind.speed)}</TableCell>
-                                <TableCell>{windDeg(day.wind.deg)}</TableCell>
-                                <TableCell>{day.wind.gust}</TableCell>
-                                <TableCell>{day.main.humidity}%</TableCell>
-                                <TableCell>{Math.floor((day.main.pressure * 0.75006156) * 100) / 100}</TableCell>
-                                <TableCell>{day.visibility / 1000}</TableCell>
-                              </TableRow>
-                            ))}
-                          </TableBody>
-                        </Table>
-                      </CardContent>
-                    </Card>
-                  </TabsContent>
-                  <TabsContent value='day5' className='mt-6'>
-                    <Card>
-                      <CardContent className='p-0'>
-                        <Table>
-                          <TableHeader>
-                            <TableRow>
-                              <TableHead>{t('table.time')}</TableHead>
-                              <TableHead>{t('table.weather')}</TableHead>
-                              <TableHead>{t('table.temperature')} (&deg;C)</TableHead>
-                              <TableHead>{t('table.feelsLike')}(&deg;C)</TableHead>
-                              <TableHead>{`${t('table.wind')} (${t('table.windSpeed')})`}</TableHead>
-                              <TableHead>{t('table.windDirection')}</TableHead>
-                              <TableHead>{t('table.windGust')}</TableHead>
-                              <TableHead>{t('table.humidity')}</TableHead>
-                              <TableHead>{t('table.pressure')} (mmHg)</TableHead>
-                              <TableHead>{`${t('table.visibility')} (${t('table.visibilityDistance')})`}</TableHead>
-                            </TableRow>
-                          </TableHeader>
-                          <TableBody>
-                            {day5.map((day: listItem, index) => (
-                              <TableRow key={index}>
-                                <TableCell className='font-medium'>{timestampConversation(day.dt)}</TableCell>
-                                <TableCell>
-                                  <div className='flex items-center space-x-2'>
-                                    {showIcon(day.weather[0].id, day.dt,'h-5 w-5 text-primary')}
-                                    <span>{day.weather[0].description}</span>
-                                  </div>
-                                </TableCell>
-                                <TableCell className='font-semibold'>{Math.round(day.main.temp)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.main.feels_like)}&deg;</TableCell>
-                                <TableCell>{Math.round(day.wind.speed)}</TableCell>
-                                <TableCell>{windDeg(day.wind.deg)}</TableCell>
-                                <TableCell>{day.wind.gust}</TableCell>
-                                <TableCell>{day.main.humidity}%</TableCell>
-                                <TableCell>{Math.floor((day.main.pressure * 0.75006156) * 100) / 100}</TableCell>
-                                <TableCell>{day.visibility / 1000}</TableCell>
-                              </TableRow>
-                            ))}
-                          </TableBody>
-                        </Table>
-                      </CardContent>
-                    </Card>
-                  </TabsContent>
+                  <DaysTabComponent
+                    dayCount="day1"
+                    day={day1}
+                    timestampConversation={timestampConversation}
+                    showIcon={showIcon}
+                    windDeg={windDeg}/>
+                  <DaysTabComponent
+                    dayCount="day2"
+                    day={day2}
+                    timestampConversation={timestampConversation}
+                    showIcon={showIcon}
+                    windDeg={windDeg}/>
+                  <DaysTabComponent
+                    dayCount="day3"
+                    day={day3}
+                    timestampConversation={timestampConversation}
+                    showIcon={showIcon}
+                    windDeg={windDeg}/>
+                  <DaysTabComponent
+                    dayCount="day4"
+                    day={day4}
+                    timestampConversation={timestampConversation}
+                    showIcon={showIcon}
+                    windDeg={windDeg}/>
+                  <DaysTabComponent
+                    dayCount="day5"
+                    day={day5}
+                    timestampConversation={timestampConversation}
+                    showIcon={showIcon}
+                    windDeg={windDeg}/>
                 </Tabs>
               </section>
             </TabsContent>
